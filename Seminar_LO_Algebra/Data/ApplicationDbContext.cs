34c73d86b1e79966a17539a62b3b1f4cf998fd21
Luka Ontl <ontlluka@gmail.com>
2023-10-31 19:47:47 +0100

    Add project files.

---
+++ Seminar_LO_Algebra/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Seminar_LO_Algebra.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Seminar_LO_Algebra/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Seminar_LO_Algebra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
