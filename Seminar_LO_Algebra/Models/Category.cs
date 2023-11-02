e3418c5cd3938fbcc8699ac3d9e4b68120bc7c69
Luka Ontl <ontlluka@gmail.com>
2023-11-02 14:52:08 +0100

    dodani modeli Category i product category

---
+++ Seminar_LO_Algebra/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Seminar_LO_Algebra.Models
+{
+    public class Category
+    {
+        [Key]
+         public int Id { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =2)]
+        public string Title { get; set; }
+
+        [ForeignKey("CategoryId")]
+        
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; } 
+
+
+    }
+}
---
+++ Seminar_LO_Algebra/Models/ProductCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Seminar_LO_Algebra.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [NotMapped]
+        public string ProductTitle { get; set; }
+
+        [NotMapped]
+        public string CategoryTitle { get; set; }
+    }
+}
